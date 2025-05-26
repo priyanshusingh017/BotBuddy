1aa3c47ab15675d518fef6042fd9de4e905d91b7
albertsingh <albert531singh@gmail.com>
2025-05-26 08:34:13 +0530

    refactor: Clean up comments and formatting in Firebase initialization script

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -1,18 +1,18 @@
-// Firebase Initialization Script
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+// Import additional Firebase services as needed
+// import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDe18gD6SF4iZSgGmKxNZNdNBZhlkrTfUg",
-    authDomain: "chatbot-43b76.firebaseapp.com",
-    projectId: "chatbot-43b76",
-    storageBucket: "chatbot-43b76.firebasestorage.app",
-    messagingSenderId: "482784554176",
-    appId: "1:482784554176:web:b2a5d7ed8f802bdfc7d125"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDe18gD6SF4iZSgGmKxNZNdNBZhlkrTfUg",
+  authDomain: "chatbot-43b76.firebaseapp.com",
+  projectId: "chatbot-43b76",
+  storageBucket: "chatbot-43b76.appspot.com",
+  messagingSenderId: "482784554176",
+  appId: "1:482784554176:web:b2a5d7ed8f802bdfc7d125",
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log("Firebase initialized:", app.name);
